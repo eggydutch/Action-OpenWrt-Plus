91bcbee2a66a296e3e7886d94a15c39fbe76626b
eggydutch <59375032+eggydutch@users.noreply.github.com>
2020-07-28 03:28:26 +0800

    Update lean-openwrt.sh

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -20,24 +20,11 @@ pushd package/community
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/SuLingGG/openwrt-package
 
-# Add mentohust & luci-app-mentohust.
-git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
-git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk
-
-# Add ServerChan
-git clone --depth=1 https://github.com/tty228/luci-app-serverchan
-
 # Add OpenClash
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 
 # Add luci-app-onliner (need luci-app-nlbwmon)
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
-
-# Add luci-app-adguardhome
-git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
-
-# Add Rclone-OpenWrt
-git clone --depth=1 https://github.com/ElonH/Rclone-OpenWrt
 
 # Add luci-app-diskman
 git clone --depth=1 https://github.com/lisaac/luci-app-diskman
@@ -65,10 +52,6 @@ svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/gotop
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/subconverter
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/jpcre2
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/rapidjson
-
-# Add smartdns
-svn co https://github.com/pymumu/smartdns/trunk/package/openwrt ../smartdns
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns ../luci-app-smartdns
 
 # Add udptools
 git clone --depth=1 https://github.com/bao3/openwrt-udp2raw
