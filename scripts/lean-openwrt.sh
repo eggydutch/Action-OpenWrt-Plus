62380f9071dd191a5be534007807480e89db7c0c
eggydutch <59375032+eggydutch@users.noreply.github.com>
2020-09-19 02:46:40 +0800

    Update lean-openwrt.sh

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -8,6 +8,15 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.8/g' package/base-files/files/bin/config_generate
 
+# Add luci-app-ssr-plus
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+
+# Add Project OpenWrt's autocore
+rm -rf autocore
+svn co https://github.com/project-openwrt/openwrt/trunk/package/lean/autocore
+popd
+
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
@@ -18,10 +27,52 @@ git clone --depth=1 https://github.com/Lienol/openwrt-package
 # Add OpenClash
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 
+# Add luci-app-onliner (need luci-app-nlbwmon)
+#git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
+
+# Add luci-app-diskman
+git clone --depth=1 https://github.com/lisaac/luci-app-diskman
+mkdir parted
+cp luci-app-diskman/Parted.Makefile parted/Makefile
+
 # Add luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 rm -rf ../lean/luci-theme-argon
 
+# Add tmate
+git clone --depth=1 https://github.com/project-openwrt/openwrt-tmate
+
+# Add gotop
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/gotop
+
+# Add udptools
+git clone --depth=1 https://github.com/bao3/openwrt-udp2raw
+git clone --depth=1 https://github.com/bao3/openwrt-udpspeeder
+git clone --depth=1 https://github.com/bao3/luci-udptools
+
 # Mod zzz-default-settings
-# sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/argon'" package/lean/default-settings/files/zzz-default-settings
-# sed -i '/http/d' package/lean/default-settings/files/zzz-default-settings
+pushd package/lean/default-settings/files
+sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/argon'" zzz-default-settings
+sed -i '/http/d' zzz-default-settings
+sed -i '/exit/i\chmod +x /bin/ipv6-helper' zzz-default-settings
+popd
+
+# Fix libssh
+pushd feeds/packages/libs
+rm -rf libssh
+svn co https://github.com/openwrt/packages/trunk/libs/libssh
+popd
+
+## Fix mt76 wireless driver
+# pushd package/kernel/mt76
+# rm -f Makefile
+# wget https://raw.githubusercontent.com/openwrt/openwrt/e12ac405525c29a6b6195e6259d769715919560c/package/kernel/mt76/Makefile
+# sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
+# popd
+
+# Add po2lmo
+git clone https://github.com/openwrt-dev/po2lmo.git
+pushd po2lmo
+make && sudo make install
+popd
